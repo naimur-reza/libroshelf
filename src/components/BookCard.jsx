ab1841b097b5bb9da20c9827cb280d6f9fd6287c
naimur-reza <naimur.rezaa@gmail.com>
2024-10-14 11:16:50 +0600

    Added search functionality

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -2,10 +2,8 @@
 const BookCard = ({ bookData }) => {
   const { title, formats, authors, name, genre, id } = bookData;
 
-  console.log(bookData);
-
   return (
-    <div key={id} className="border border-gray-700/80 p-4 h-96">
+    <div key={id} className="border border-gray-700/80 p-4 h-80">
       {/* <h1>Name:{name}</h1> */}
       {/* <h1>Genre:{genre}</h1> */}
       {/* <h1>Authors:{authors.map((auth) => auth.name)}</h1> */}
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,29 +1,62 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import BookCard from "./BookCard";
 import Loading from "./Loading";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+  const [filter, setFilter] = useState("");
+
+  // Debounce search input
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearch(search);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search]);
 
   useEffect(() => {
-    fetch("https://gutendex.com/books")
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data);
-        setLoading(false);
-      });
-  }, []);
+    setLoading(true);
+    try {
+      fetch(
+        `https://gutendex.com/books?search=${debouncedSearch}&topic=${filter}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setBooks(data);
+          setLoading(false);
+        });
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [debouncedSearch, filter]);
 
-  if (loading) return <Loading />;
+  console.log(debouncedSearch, loading);
 
   return (
-    <div className="mt-10 grid grid-cols-4 gap-3 max-w-6xl mx-auto">
-      {books &&
-        books?.results?.map((book) => (
-          <BookCard key={book.id} bookData={book} />
-        ))}
+    <div className="mt-10 max-w-6xl mx-auto">
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Search books"
+        className="my-5 px-4 py-3 border border-gray-700/80 rounded-full caret-blue-500 text-gray-200"
+      />
+
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="grid grid-cols-4 gap-3">
+          {books?.results?.map((book) => (
+            <BookCard key={book.id} bookData={book} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
