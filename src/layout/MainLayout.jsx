08039646bac6b9ba37a9a321615ed8a9a76c5eaa
naimur-reza <naimur.rezaa@gmail.com>
2024-10-15 16:00:39 +0600

    Fix layout color

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Shared/Navbar";
 
 const MainLayout = () => {
   return (
-    <div className="bg-[#181C14]">
+    <div className="bg-[#10130e]">
       <Navbar />
       <div className="min-h-[calc(100vh-120px)]">
         <Outlet />
